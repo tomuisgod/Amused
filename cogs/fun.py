0d2f6638c70bd2babd98fe56b8fc6a6af5d40df2
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-07-25 14:05:51 +0200

    Cogs

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,4 +1,6 @@
 import discord
+import requests
+import json
 from discord.ext import commands
 
 client = discord.Client()
@@ -31,47 +33,49 @@ iqs = [
 
 ]
 
-
-
 class Fun(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
-        # loaded
-        @commands.Cog.listener()
-        async def on_ready(self):
-            print("Fun.py loaded")
+    # loaded
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Fun.py loaded")
 
-        @commands.command()
-        async def pp(self, ctx):
-            await ctx.send("This is your pp: " + random.choice(pps))
+    # commands
 
-        @commands.command()
-        async def iq(self, ctx):
-            await ctx.send(random.choice(iqs))
+    @commands.command()
+    async def pp(self, ctx):
+        await ctx.send("This is your pp: " + random.choice(pps))
 
-        @commands.command(aliases=["8b"])
-        async def _8ball(self, ctx, *, question):
-            responses = [
-                "Sure!",
-                "Nahhh!",
-                "I don't know dude!",
-                "100%!!",
-                "Not a chance!",
-                "Ask me later please!",
-                "No.",
-                "Yes.",
-                "It's possible!",
-                "I can't answer this right now!"
-            ]
+    @commands.command()
+    async def iq(self, ctx):
+        await ctx.send(random.choice(iqs))
 
-            embed = discord.Embed(title="**8ball**", color=discord.Color.blurple(), timestamp=ctx.message.created_at)
-            embed.add_field(name=" Q&A ",
-                            value=f"**Question:** {question}\n **Answer:** {random.choice(responses)}")
-            embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
+    @commands.command(aliases=["8b"])
+    async def _8ball(self, ctx, *, question):
+        responses = [
+            "Sure!",
+            "Nahhh!",
+            "I don't know dude!",
+            "100%!!",
+            "Not a chance!",
+            "Ask me later please!",
+            "No.",
+            "Yes.",
+            "It's possible!",
+            "I can't answer this right now!"
+        ]
 
-            await ctx.send(embed=embed)
+        embed = discord.Embed(title="**8ball**", color=discord.Color.blurple(), timestamp=ctx.message.created_at)
+        embed.add_field(name=" Q&A ",
+                        value=f"**Question:** {question}\n **Answer:** {random.choice(responses)}")
+        embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
+
+        await ctx.send(embed=embed)
+
+
 
 def setup(client):
     client.add_cog(Fun(client))
