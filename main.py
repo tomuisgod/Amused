05cd6772edf3493817d4fe493f70edc196653c58
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-07-25 14:01:34 +0200

    Main file

--- main.py
+++ main.py
@@ -20,33 +20,7 @@ TOKEN = os.getenv('TOKEN')
 with open('../../PycharmProjects/tomu/words.txt') as file:
     file = file.read().split()
 
-pps = [
-    "You don't have one!",
-    "8=D",
-    "8==D",
-    "8===D",
-    "8====D",
-    "8======D",
-    "8=======D",
-    "8=========D",
-    "8===========D",
-    "8================D",
-    "8===================D",
-    "8===============================D  (largets possible :hot_face:)"
-]
 
-
-iqs = [
-    "Small IQ!",
-    "Very small IQ!",
-    "Your so dumb dude I don't honestly know If you can even read!",
-    "Average IQ!",
-    "Pretty good IQ!",
-    "Your so smart!",
-    "High IQ!",
-    "Very high IQ!"
-
-]
 
 
 
@@ -76,37 +50,6 @@ async def on_command_error(ctx, error):
         embed = discord.Embed(title=":x: **Bot don't have enough permission to execute this command!**", color=discord.Color.red())
         await ctx.send(embed=embeda)
 
-
-@client.command()
-async def pp(ctx):
-    await ctx.send("This is your pp: " + random.choice(pps))
-
-@client.command()
-async def iq(ctx):
-    await ctx.send(random.choice(iqs))
-
-
-@client.command(aliases=["8b"])
-async def _8ball(ctx, *, question):
-    responses = [
-        "Sure!",
-        "Nahhh!",
-        "I don't know dude!",
-        "100%!!",
-        "Not a chance!",
-        "Ask me later please!",
-        "No.",
-        "Yes.",
-        "It's possible!",
-        "I can't answer this right now!"
-    ]
-
-    embed = discord.Embed(title="**8ball**", color=discord.Color.blurple(), timestamp=ctx.message.created_at)
-    embed.add_field(name=" Q&A ",
-                    value=f"**Question:** {question}\n **Answer:** {random.choice(responses)}")
-    embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
-
-    await ctx.send(embed=embed)
 
 
 @client.command()
