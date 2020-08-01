f5deab19521da59a2fb809e5e04d9147492f3c62
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-08-01 19:13:48 +0200

    Cogs

--- cogs/help.py
+++ cogs/help.py
@@ -20,10 +20,12 @@ class Help(commands.Cog):
         await ctx.send(":white_check_mark: | I sent you DM with help command!")
 
         embed = discord.Embed(title=" ", colour=discord.Color.blurple())
-        embed.set_author(name="My prefix is / and I have 16 commands!", icon_url=self.client.user.avatar_url)
+        embed.set_author(name="My prefix is / and I have 25 commands!", icon_url=self.client.user.avatar_url)
         embed.add_field(name=":monkey_face: Fun", value="`pp` | `iq` | `8b` | `say` ", inline=False)
+        embed.add_field(name=":couple: Interactions", value="`dance` | `hug` | `kiss` | `bully` | `cry` | `pat` | "
+                                                            "`highfive` | `bite` | `slap`", inline=False)
         embed.add_field(name=":gear: Information", value="`help` | `info`", inline=False)
-        embed.add_field(name=":frame_photo: Photos", value="`waifus` | `shiba` | `dog` | `skin`", inline=False)
+        embed.add_field(name=":frame_photo: Photos", value="`waifus` | `shiba` | `dog` | `skin` | `meme`", inline=False)
         embed.add_field(name=":hammer: Administration", value="`ban` | `kick` | `clear`", inline=False)
         embed.add_field(name=":test_tube: Development", value="`load` | `unload` | `reload`")
         await ctx.author.send(embed=embed)
