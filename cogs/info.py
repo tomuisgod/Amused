2eda02b777f6604e720af1cd355cf57d9eaacd0a
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-08-01 11:38:39 +0200

    Cogs

--- cogs/info.py
+++ cogs/info.py
@@ -20,7 +20,7 @@ class Info(commands.Cog):
         embed = discord.Embed(title=":gear: **Info**", color=discord.Color.blurple())
         embed.add_field(name="`Ping:` ", value=f"**{round(self.client.latency * 1000)}** ms", inline=False)
         embed.add_field(name="`Discord.py version: `", value=f"**{discord.__version__}**", inline=False)
-        embed.add_field(name="`Bot version:` ", value="**0.1**", inline=False)
+        embed.add_field(name="`Bot version:` ", value="**0.1.1**", inline=False)
         embed.set_footer(text=f"{self.client.user.name}",
                          icon_url=self.client.user.avatar_url)
         await ctx.send(embed=embed)
