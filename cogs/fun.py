2c4786b5be6bd7e7c663c02a3b21fb53d839abce
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-08-27 10:42:50 +0200

    Cogs

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,7 @@
 import discord
 import requests
 import json
+import random
 from discord.ext import commands
 
 client = discord.Client()
@@ -22,15 +23,37 @@ pps = [
 
 
 iqs = [
-    "Small IQ!",
-    "Very small IQ!",
-    "Your so dumb dude I don't honestly know If you can even read!",
-    "Average IQ!",
-    "Pretty good IQ!",
-    "Your so smart!",
-    "High IQ!",
-    "Very high IQ!"
+    "...small!",
+    "...very small!",
+    "...your so dumb dude I don't honestly know If you can even read!",
+    "...average!",
+    "...pretty good!",
+    "...your so smart that I can't even measure your IQ!",
+    "...high!",
+    "...very high!"
 
+]
+
+love = [
+    "0%"
+    "32%"
+    "48%"
+    "15%"
+    "87%"
+    "47%"
+    "14%"
+    "0%"
+    "41%"
+    "100%"
+    "49%"
+    "78%"
+    "12%"
+    "1%"
+    "26%"
+    "36%"
+    "46%"
+    "76%"
+    "44%"
 ]
 
 class Fun(commands.Cog):
@@ -47,11 +70,20 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def pp(self, ctx):
-        await ctx.send("This is your pp: " + random.choice(pps))
+        embed = discord.Embed(title="Pp meter", color=discord.Color.blurple(), timestamp=ctx.message.created_at)
+        embed.add_field(name="This is your penis", value=f"{random.choice(pps)}")
+        embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
+
+        await ctx.send(embed=embed)
+
 
     @commands.command()
     async def iq(self, ctx):
-        await ctx.send(random.choice(iqs))
+        embed = discord.Embed(title="IQ meter", color=discord.Color.blurple(), timestamp=ctx.message.created_at)
+        embed.add_field(name="Your IQ is...", value=f"{random.choice(iqs)}")
+        embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
+
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=["8b"])
     async def _8ball(self, ctx, *, question):
@@ -75,7 +107,11 @@ class Fun(commands.Cog):
 
         await ctx.send(embed=embed)
 
-
+    @_8ball.error
+    async def _8ball_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(title="**Missing Argument! Please use correct form** `/8ball <question>`", color=discord.Color.red())
+            await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Fun(client))
