f97f8ceca8d58b9cdcd9b063af45849f3ddab94c
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-08-27 10:41:12 +0200

    Cog for info command

--- cogs/info.py
+++ cogs/info.py
@@ -17,10 +17,11 @@ class Info(commands.Cog):
     # command
     @commands.command()
     async def info(self, ctx):
-        embed = discord.Embed(title=":gear: **Info**", color=discord.Color.blurple())
+        embed = discord.Embed(title="<:4228_discord_bot_dev:748426660478451803> **Info**", color=discord.Color.blurple())
         embed.add_field(name="`Ping:` ", value=f"**{round(self.client.latency * 1000)}** ms", inline=False)
         embed.add_field(name="`Discord.py version: `", value=f"**{discord.__version__}**", inline=False)
-        embed.add_field(name="`Bot version:` ", value="**0.1.1**", inline=False)
+        embed.add_field(name="`Bot version:` ", value="**0.1.2**", inline=False)
+        embed.add_field(name="`Bot developer:`", value="**bomu#1979**")
         embed.set_footer(text=f"{self.client.user.name}",
                          icon_url=self.client.user.avatar_url)
         await ctx.send(embed=embed)
