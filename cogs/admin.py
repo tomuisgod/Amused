32e3f1f469a5a80ae4ea37021c30ea79fef4420d
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-08-27 10:42:17 +0200

    Cogs

--- cogs/admin.py
+++ cogs/admin.py
@@ -17,28 +17,43 @@ class Admin(commands.Cog):
     # command
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(ctx, member: discord.Member, *, reason=None):
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
         await member.ban(reason=reason)
         embed = discord.Embed(title=":hammer: **Member has been banned!**", colour=discord.Color.red(), timestamp=ctx.message.created_at)
         embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=embed)
 
+    @ban.error
+    async def ban_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed= discord.Embed(title="**Missing Argument! Please use correct form** `/ban @mention`", color=discord.Color.red())
+
+            await ctx.send(embed=embed)
+
     @commands.command()
     @commands.has_permissions(kick_members=True)
-    async def kick(ctx, member: discord.Member, *, reason=None):
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
         await member.kick(reason=reason)
         embed = discord.Embed(title=":hammer: **Member has been kicked!**", color=discord.Color.red(), timestamp=ctx.message.created_at)
         embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=embed)
 
+    @kick.error
+    async def kick_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(title="**Missing Argument! Please use correct form** `/kick @mention`",
+                                  color=discord.Color.red())
+
+            await ctx.send(embed=embed)
+
     @commands.command()
-    @commands.has_permissions(manage_messages=True)
-    async def clear(ctx, amount=10):
-        await ctx.channel.purge(limit=amount)
-        embed = discord.Embed(title=":white_check_mark: **Selected amount of messages has been deleted!**",
-                              color=discord.Color.green())
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, member:discord.Member, *, reason=None):
+        await member.unban(reason=reason)
+        embed = discord.Embed(title=":hammer: **Member has been unbaned!**", color=discord.Color.red(), timestamp=ctx.message.created_at)
+        embed.set_footer(text="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=embed)
 
