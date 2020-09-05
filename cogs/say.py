9a6488bb7b2924c713f99944ece4c6c35273e790
Pitisko <66744640+Pitisko@users.noreply.github.com>
2020-09-05 13:32:58 +0200

    Cogs

--- cogs/say.py
+++ cogs/say.py
@@ -15,6 +15,7 @@ class Say(commands.Cog):
 
     # command
     @commands.command()
+    @commands.has_permissions(manage_messages=True)
     async def say(self, ctx, *, texts, amount=1):
         await ctx.channel.purge(limit=amount)
 
@@ -22,6 +23,15 @@ class Say(commands.Cog):
         embed.set_author(name="{}".format(ctx.message.author.name), icon_url=ctx.author.avatar_url)
 
         await ctx.send(embed=embed)
+
+    @say.error
+    async def say_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(title="<:9830_no:748426943766069308> **Missing Argument! Please use correct form** `/say <text>`",
+                                  color=discord.Color.red())
+
+            await ctx.send(embed=embed)
+
 
 def setup(client):
     client.add_cog(Say(client))
